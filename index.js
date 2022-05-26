ae22272526d03e2bc33ebab64cc0ca1691361a6d
xerxex-odame <kingodame@yahoo.com>
2022-05-26 10:23:16 +0000

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// code your solution here
+const superbowlWin = (objArray)=>{
+    let objFind = objArray.find((item)=>item.result === "W")
+    if(objFind != undefined){
+      return objFind.year;
+    } else {
+      return undefined;
+    }
+  }
+  
+  superbowlWin(objArray);
